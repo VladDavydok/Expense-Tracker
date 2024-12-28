a347e7e7d68c320ff9dc127c241ce35eef8ddce4
VladDavydok <davidok.vlad@gmail.com>
2024-12-28 17:48:38 +0200

    Eight commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,42 @@
 import { useState } from 'react';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import SignUp from './pages/sign-up.jsx';
+import useStore from './store';
+import { setAuthToken } from './libs/apiCall';
+import { Toaster } from 'sonner';
+
+const RootLayout = () => {
+  const { user } = useStore((state) => state);
+
+  setAuthToken(user?.token || '');
+
+  return !user ? (
+    <Navigate to="sign-in" replace={true} />
+  ) : (
+    <>
+      <Navbar />
+      <div className="min-h-[cal(h-screen-100px)]">
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <main>
+      <div className="w-full min-h-screen px-6 bg-gray-100 md:px-20 dark:bg-slate-900">
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Navigate to="/overview" />} />
+          </Route>
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </div>
 
-  return <h1 className="font-bold text-5xl">Hello</h1>;
+      <Toaster richColors position="top-center" />
+    </main>
+  );
 }
 
 export default App;
