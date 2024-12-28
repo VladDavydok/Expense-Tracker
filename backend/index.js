39267c318246aeaef43ae22fb69b9545d10fd919
VladDavydok <davidok.vlad@gmail.com>
2024-12-28 13:59:47 +0200

    Third commit

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ app.use(cors('*'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api-v1', routes);
+
 app.use('*', (req, res) => {
   res.status(404).json({
     status: '404 Not found',
