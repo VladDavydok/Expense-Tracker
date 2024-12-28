ae44f6027518af7853f4bc501e0a503a3b546a18
VladDavydok <davidok.vlad@gmail.com>
2024-12-28 21:49:01 +0200

    Fifteenth commit

--- frontend/src/libs/apiCall.js
+++ frontend/src/libs/apiCall.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api-v1';
+const API_URL = 'https://expense-tracker-8y1g.onrender.com:5000/api-v1';
 
 const api = axios.create({
   baseURL: API_URL,
